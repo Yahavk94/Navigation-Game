6a8e89b26aec03958ee82e1bf4a0c5b35bef0497
Yahavk94 <yahavk94@gmail.com>
2020-01-19 22:27:28 +0200

    Final commit

--- src/gameGUI/AutomatedTest.java
+++ src/gameGUI/AutomatedTest.java
@@ -1,13 +1,44 @@
 package gameGUI;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import Server.Game_Server;
+import Server.game_service;
+import dataStructure.DGraph;
+import gameData.Fruit;
 
 class AutomatedTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetGameFruits() {
+		DGraph gameGraph = new DGraph();
+		game_service myGame = Game_Server.getServer(0);
+		gameGraph.init(myGame.getGraph());
+		List<String> fruitsList = myGame.getFruits();
+		assertEquals(fruitsList.size(), 1);
+		Fruit newFruit = new Fruit(fruitsList.get(0), gameGraph);
+		assertEquals(newFruit.getType(), -1); // Banana
 	}
-
+	
+	void testGetBestFruit() {
+		DGraph gameGraph = new DGraph();
+		game_service myGame = Game_Server.getServer(0);
+		gameGraph.init(myGame.getGraph());
+		ArrayList<Fruit> emptyList = new ArrayList<>();
+		ArrayList<Fruit> gameFruits = Automated.getGameFruits(emptyList, gameGraph, myGame);
+		assertEquals(gameFruits.get(0), Automated.getBestFruit(gameFruits));
+	}
+	
+	void removeBestFruit() {
+		DGraph gameGraph = new DGraph();
+		game_service myGame = Game_Server.getServer(0);
+		gameGraph.init(myGame.getGraph());
+		ArrayList<Fruit> emptyList = new ArrayList<>();
+		ArrayList<Fruit> gameFruits = Automated.getGameFruits(emptyList, gameGraph, myGame);
+		gameFruits = Automated.removeBestFruit(gameFruits, gameFruits.get(0));
+		assertEquals(gameFruits.size(), 0);
+	}
 }
